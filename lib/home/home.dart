81fa99ee9f5c93210d21b2e8274b473b411c458c
Vincent-Davis <leonardvincent62@gmail.com>
2024-12-05 13:20:23 +0700

    api endpoint works

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import '../search/screens/list_gameentry.dart';
 import '../userprofile/screens/user_profile_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,16 +15,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CupertinoButton(
-          color: Colors.grey,
-          child: const Text("User Profile"), 
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const UserProfilePage()),
-              );
-          }
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoButton(
+              color: Colors.grey,
+              child: const Text("User Profile"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const UserProfilePage(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 16), // Jarak antar tombol
+            CupertinoButton(
+              color: Colors.blue,
+              child: const Text("Mood Entry List"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MoodEntryPage(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
