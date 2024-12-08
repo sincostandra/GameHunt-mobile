e0301b9a938ac35a5dc6a4dc0f3f3c3648c2e604
alicfamahdi <alicfamahdi@gmail.com>
2024-12-08 12:09:48 +0700

    feat: newsbox widget

--- lib/news/screens/news_page.dart
+++ lib/news/screens/news_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gamehunt/news/models/news_model.dart';
+import 'package:gamehunt/news/widgets/news_box.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart'; 
 import 'package:provider/provider.dart';
 import 'package:gamehunt/news/screens/news_detail.dart'; 
@@ -69,40 +70,7 @@ class _NewsPageState extends State<NewsPage> {
           ),
         );
       },
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        padding: const EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 5,
-              offset: Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              news.fields.title,
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(news.fields.author),
-            const SizedBox(height: 10),
-            Text(news.fields.updateDate),
-            const SizedBox(height: 10),
-            Text(news.fields.article),
-          ],
-        ),
-      ),
+      child: NewsBox(news: news)
     );
   },
 );
